2996bb831fb51fe7c7e7b4462488f142c8391156
Spherz <spherzpro228@gmail.com>
2024-04-23 00:56:22 +0200

    add test data for db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO post_office(id, address, postal_code) VALUES (1, 'г Калининград, ул 9 Апреля, дом 7', '236016');
+INSERT INTO post_office(id, address, postal_code) VALUES (2, 'г Москва, ул Большая Грузинская, дом 37', '980405');
+INSERT INTO post_office(id, address, postal_code) VALUES (3, 'г Москва, ш Перовское, дом 18/1', '109202');
+INSERT INTO post_office(id, address, postal_code) VALUES (4, 'г Котельники, тер Новорязанское шоссе, дом 6 стр. 4М', '982074');
+INSERT INTO post_office(id, address, postal_code) VALUES (5, 'г Санкт-Петербург, пр-кт КИМа, дом 4 литера ', '199155');
+INSERT INTO post_office(id, address, postal_code) VALUES (6, 'г Санкт-Петербург, пр-кт Полюстровский, дом 45 литера А', '983006');
+INSERT INTO post_office(id, address, postal_code) VALUES (7, 'г Санкт-Петербург, пр-кт Гражданский, дом 114 литера А к. 1', '195267');
+INSERT INTO post_office(id, address, postal_code) VALUES (8, 'г Санкт-Петербург, дор Торфяная, дом 7 литера А', '983190');
+INSERT INTO post_office(id, address, postal_code) VALUES (9, 'г Калининград, ул В.Денисова, дом 16 к. 1', '236048');
+INSERT INTO post_office(id, address, postal_code) VALUES (10, 'г Калининград, ул Черняховского, дом 56', '236021');
